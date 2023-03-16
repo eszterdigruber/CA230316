6d68526815451ff741efb03d84300fc4c76bf4a9
Eszter Digruber <digruber.eszter@verebelyszki.hu>
2023-03-16 14:29:25 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -9,6 +9,27 @@ namespace HelloWorld
         {
             Feladat4Beolvasas(); Console.WriteLine("\n----------------------------\n");
             Feladat5(); Console.WriteLine("\n----------------------------\n");
+            Feladat6(); Console.WriteLine("\n----------------------------\n");
+        }
+
+        private static void Feladat6()
+        {
+            Console.WriteLine("6.feladat: Bekérés + keresés");
+            Console.Write("Kérem adjon meg egy nevet: ");
+            string KeresNev = Console.ReadLine();
+            int db = 0;
+            foreach (var v in Vizi_List)
+            {
+                if (KeresNev == v.Nev)
+                {
+                    db++;
+                    Console.WriteLine($"{v.Eora}:{v.Eperc} - {v.Vora}:{v.Vperc}");
+                }
+            }
+            if (db == 0)
+            {
+                Console.WriteLine("Nem volt ilyen nevű kölcsönző.");
+            }
         }
 
         private static void Feladat5()
