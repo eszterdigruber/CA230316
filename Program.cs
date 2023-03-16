015c16607cb51e4ddeb42a2ed08a55a49799e431
Eszter Digruber <digruber.eszter@verebelyszki.hu>
2023-03-16 14:03:42 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
-﻿namespace HelloWorld
+﻿using System.IO;
+using System.Text;
+namespace HelloWorld
 {
     class Program
     {
+        static List<Vizibicikli> Vizi_List = new List<Vizibicikli>();
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
---
+++ Vizibicikli.cs
@@ -0,0 +1,19 @@
+﻿namespace HelloWorld
+{
+    internal class Vizibicikli
+    {
+        //Név;JAzon;EÓra;EPerc;VÓra;Vperc
+        public string Nev;
+        public string JAzon;
+        public int Eora;
+        public int Eperc;
+        public int Vora;
+        public int Vperc;
+
+        public Vizibicikli (string Sor)
+        {
+            var dbok = Sor.Split(';');
+
+        }
+    }
+}
