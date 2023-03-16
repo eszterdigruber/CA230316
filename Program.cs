e4661d870b31d6a1116f2d4348bf381165a29047
Eszter Digruber <digruber.eszter@verebelyszki.hu>
2023-03-16 14:13:24 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -7,7 +7,18 @@ namespace HelloWorld
         static List<Vizibicikli> Vizi_List = new List<Vizibicikli>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Feladat3Beolvasas(); Console.WriteLine("\n----------------------------\n");
+        }
+
+        private static void Feladat3Beolvasas()
+        {
+            Console.WriteLine("3.feladat: Beolvasás");
+            var sr = new StreamReader(@"kolcsonzesek.txt", Encoding.UTF8);
+            while (!sr.EndOfStream)
+            {
+                Vizi_List.Add(new Vizibicikli(sr.ReadLine()));
+            }
+            sr.Close();
         }
     }
 }
